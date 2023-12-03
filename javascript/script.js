f3694186216d2a5a97ea9bcdfd7b3bd75edfc399
hendrisprta <146816124+hendrisprta@users.noreply.github.com>
2023-12-03 22:20:58 +0700

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,31 @@
 //JAVASCRIPT RESET FORM
 function resetFormLuas(){
-    document.getElementById("input-alas").value="";
-    document.getElementById("input-tinggi").value="";
+    document.getElementById("inputAlas").value="";
+    document.getElementById("inputTinggi").value="";
+    document.getElementById("resultLuas").textContent="Luas Segitiga adalah : "+ luas;
+
 }
 
 function resetFormKeliling(){
-    document.getElementById("input-sisi-1").value="";
-    document.getElementById("input-sisi-2").value="";
-    document.getElementById("input-sisi-3").value="";
+    document.getElementById("inputSisi1").value="";
+    document.getElementById("inputSisi2").value="";
+    document.getElementById("inputSisi3").value="";
+    document.getElementById("resultKeliling").textContent="";
+}
+//JAVASCRIPT KALKULATOR KELILING SEGITIGA
+function kalkulatorKeliling(){
+    var inputSisi1 = parseFloat(document.getElementById("inputSisi1").value);
+    var inputSisi2 = parseFloat(document.getElementById("inputSisi2").value);
+    var inputSisi3 = parseFloat(document.getElementById("inputSisi3").value);
+
+    var keliling = inputSisi1 + inputSisi2 + inputSisi3;
+    document.getElementById("resultKeliling").textContent="Keliling Segitiga adalah : "+ keliling;
+}
+//JAVASCRIPT LUAS SEGITIGA
+function kalkulatorLuas(){
+    var inputAlas = parseFloat(document.getElementById("inputAlas").value);
+    var inputTinggi = parseFloat(document.getElementById("inputTinggi").value);
+
+    var luas = 1/2 * (inputAlas * inputTinggi);
+    document.getElementById("resultLuas").textContent="Luas Segitiga adalah : "+ luas;
 }
