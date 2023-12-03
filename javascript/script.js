08a476b5f99d579a86cc5e2bc8672e9e5b568192
hendrisprta <146816124+hendrisprta@users.noreply.github.com>
2023-12-03 13:27:40 +0700

    Update script.js

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+//JAVASCRIPT RESET FORM
+function resetFormLuas(){
+    document.getElementById("input-alas").value="";
+    document.getElementById("input-tinggi").value="";
+}
+
+function resetFormKeliling(){
+    document.getElementById("input-sisi-1").value="";
+    document.getElementById("input-sisi-2").value="";
+    document.getElementById("input-sisi-3").value="";
+}
